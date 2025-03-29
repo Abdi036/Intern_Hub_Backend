fae89bc784f9d9bc2cde0ba55612272500ae05ec
Abdi036 <abdikumela036@gmail.com>
2025-03-28 19:28:44 -1200

    Refactor authentication middleware and routes; remove unused error handling and async utility

--- Server/routes/authRoute.js
+++ Server/routes/authRoute.js
@@ -1,8 +1,1 @@
-const express = require("express");
-const router = express.Router();
-const { Signin, Signup } = require(".././middleware/authController");
-
-router.post("/signup", Signup);
-router.post("/signin", Signin);
-
-module.exports = router;
+ 
--- Server/server.js
+++ Server/server.js
@@ -3,14 +3,12 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
-const errorMiddleware = require("./controllers/errorController");
-const authRoute = require("./routes/authRoute");
 
 dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
 
 // Database Connection
@@ -20,12 +18,10 @@ mongoose
   .catch((error) => console.error("DB connection failed:", error.message));
 
 // Routes
-app.use("api/v1/auth", authRoute);
 
 // Global Error Handling Middleware
-app.use(errorMiddleware);
 
 // Server
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running...`);
+  console.log(`Server is running on port ${process.env.PORT}...`);
 });
