c2d96a688d2e10732385b24a205e7dcca0c487f1
Abdi036 <abdikumela036@gmail.com>
2025-04-03 23:16:06 -1200

    initialized my app

---
+++ Client/app/_layout.tsx
@@ -0,0 +1,12 @@
+import "../global.css";
+
+import { Text, View } from 'react-native'
+import React from 'react'
+
+export default function RootLayout() {
+  return (
+    <View>
+      <Text>RootLayout</Text>
+    </View>
+  )
+}
